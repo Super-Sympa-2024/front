89c1975c1948cc6d31bdae093f1a4a063b94a65a
Léo <leo.coste@etu.univ-lyon1.fr>
2024-12-06 06:50:24 +0100

    mise a jour du captcha

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react'
 import { createLazyFileRoute } from '@tanstack/react-router'
 
-import CapchatComponent from '@components/CapchatComponent'
-
 export const Route = createLazyFileRoute('/')({
   component: RouteComponent
 })
 
 function RouteComponent() {
-  return <CapchatComponent />
+  return 'Hello /!'
 }
